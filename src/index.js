f16ccb397449b0c5e6be39d4db71e98bd062c118
leonard33 <leonardkombo14@gmail.com>
2022-05-24 07:10:45 +0300

    project setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// css files here
+import './style.css';
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=+city&units=metric&';
+const appId = '8e08d85c43cbbcb562944120f30fb25d';
